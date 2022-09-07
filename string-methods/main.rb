6f653141e925efc2fdfb82ae5998426081800b2d
Gaiya M. Obed <gaiyaobed94@gmail.com>
2022-09-07 04:40:42 +0100

    Working with string methos

---
+++ string-methods/main.rb
@@ -0,0 +1,7 @@
+name = "i am \"john\" doe                    "
+ phrase = "Best guy"
+puts phrase.downcase()
+puts phrase.upcase()
+puts name.strip()
+puts name.length()
+puts phrase.include? "Best"
