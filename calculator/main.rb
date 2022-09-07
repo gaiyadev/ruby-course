b6f78708291eb969c8997b418f3a777a13e3da57
Gaiya M. Obed <gaiyaobed94@gmail.com>
2022-09-07 09:46:16 +0100

    rewrite calculator using functiona

--- calculator/main.rb
+++ calculator/main.rb
@@ -4,4 +4,35 @@ num1 = gets.chomp().to_f
 puts "Enter another number: "
 num2 = gets.chomp().to_f
 
-puts (num1 + num2)
+puts "Enter operator: "
+operator = gets.chomp()
+
+
+def add(num1, num2)
+    return  puts num1 + num2
+  end
+  
+def substract(num1, num2)
+      return puts num1 - num2
+end
+  
+def multiple(num1, num2)
+      return puts  num1 * num2
+  end
+  
+def divide(num1, num2)
+      return puts  num1 / num2
+end
+
+
+if operator == "+"
+    add(num1, num1)
+elsif operator == "-"
+    substract(num1, num2)
+elsif operator == "/"
+    divide(num1, num2)
+elsif operator == "*"
+    multiple(num1, num2)
+end
+
+
