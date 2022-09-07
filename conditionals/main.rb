94b35a26608ee96ea22b9b0e8180df5ee8e68183
Gaiya M. Obed <gaiyaobed94@gmail.com>
2022-09-07 09:14:46 +0100

    conditionl states

---
+++ conditionals/main.rb
@@ -0,0 +1,10 @@
+age = 2
+name = "John"
+#> >= < <= != ==
+if age > 9 or name == "john"
+    puts "Older"
+elsif age < 9
+    puts "Too young"   
+elsif age == 9
+    puts "perfect"
+end
---
+++ functions/main.rb
@@ -0,0 +1,5 @@
+def sayHi(name="no name", age = -2)
+    puts "Hi " + name + "your are " +age.to_s
+end
+
+sayHi("Doe")
