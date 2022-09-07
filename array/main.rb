6b343cef6c26bf2b72e4513249e59c8cfcb242dc
Gaiya M. Obed <gaiyaobed94@gmail.com>
2022-09-07 08:49:32 +0100

    hashes

---
+++ array/main.rb
@@ -0,0 +1,6 @@
+
+ages = [4, 6, 8, 88, 7]
+ages.append(46)
+ages.pop()
+puts ages
+# Ruby array share alot of similarities with JS array
---
+++ hashes/main.rb
@@ -0,0 +1,2 @@
+person = {name: "john Doe", age: 45, code: "NY"}
+puts person[:name]
