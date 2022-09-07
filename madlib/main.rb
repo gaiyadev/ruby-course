4747d7348b66b620db3a9d30894ab2e80c5695b5
Gaiya M. Obed <gaiyaobed94@gmail.com>
2022-09-07 08:21:58 +0100

    Exercises

---
+++ madlib/main.rb
@@ -0,0 +1,12 @@
+puts "Enter a color"
+color = gets.chomp()
+
+puts "Enter a plural"
+plural = gets.chomp()
+
+puts "Enter a celebrity"
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural + "Roses are blue")
+puts ("I love " + celebrity)
